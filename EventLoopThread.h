d0977ad663f6b5a7f5398d4081216f20deb9eaba
jieL <sy1701129@buaa.edu.cn>
2019-05-15 10:05:05 +0800

    web-sever v1.0

---
+++ EventLoopThread.h
@@ -0,0 +1,27 @@
+#ifndef EVENTLOOPTHREAD_H_INCLUDED
+#define EVENTLOOPTHREAD_H_INCLUDED
+
+#pragma once
+#include "base/Condition.h"
+#include "base/MutexLock.h"
+#include "base/Thread.h"
+#include "base/noncopyable.h"
+#include "EventLoop.h"
+
+class EventLoopThread :noncopyable
+{
+public:
+    EventLoopThread();
+    ~EventLoopThread();
+    EventLoop* startLoop();
+
+private:
+    void threadFunc();
+    EventLoop *loop_;
+    bool exiting_;
+    Thread thread_;
+    MutexLock mutex_;
+    Condition cond_;
+};
+
+#endif // EVENTLOOPTHREAD_H_INCLUDED

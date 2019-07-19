48ddd06f1504b040ebb5f193c35de763ee8603de
jieL <sy1701129@buaa.edu.cn>
2019-07-19 13:16:07 +0800

    creat main.cpp

---
+++ Main.cpp
@@ -0,0 +1,50 @@
+#include "EventLoop.h"
+#include "Server.h"
+#include "base/Logging.h"
+#include <getopt.h>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+    int threadNum = 4;
+    int port = 20001;
+    std::string logPath = "./WebServer.log";
+
+    // parse args
+    int opt;
+    const char *str = "t:l:p:";
+    while ((opt = getopt(argc, argv, str))!= -1)
+    {
+        switch (opt)
+        {
+            case 't':
+            {
+                threadNum = atoi(optarg);
+                break;
+            }
+            case 'l':
+            {
+                logPath = optarg;
+                if (logPath.size() < 2 || optarg[0] != '/')
+                {
+                    printf("logPath should start with \"/\"\n");
+                    abort();
+                }
+                break;
+            }
+            case 'p':
+            {
+                port = atoi(optarg);
+                break;
+            }
+            default: break;
+        }
+    }
+    Logger::setLogFileName(logPath);
+    
+    EventLoop mainLoop;
+    Server myHTTPServer(&mainLoop, threadNum, port);
+    myHTTPServer.start();
+    mainLoop.loop();
+    return 0;
+}
